6abf196bf563e09e07868c665df91f06108bcff4
sarah116 <saraahadzic@gmail.com>
2015-04-07 19:29:34 +0200

    Uradjena javadoc dokumentacija klase Twitter

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,12 +1,39 @@
 package com.twitter.poruke;
 
+/**
+ * 
+ * @author Sara
+ * @version 1.0 prva verzija koda
+ * 
+ */
+
 public class TwitterPoruka {
+
+	/**
+	 * U klasu ubaceni atributi:
+	 * <ul>
+	 * <li>korisnik
+	 * <li>poruka
+	 * </ul>
+	 */
+
 	private String korisnik;
 	private String poruka;
+
+	/**
+	 * 
+	 * @return metoda vraca vrednost atributa korisnik
+	 */
 
 	public String getKorisnik() {
 		return korisnik;
 	}
+
+	/**
+	 * 
+	 * @param korisnik
+	 *            metoda podesava vrednost atributa korisnik na unetu vrednost
+	 */
 
 	public void setKorisnik(String korisnik) {
 		if (korisnik == null || korisnik.equals(""))
@@ -14,9 +41,20 @@ public class TwitterPoruka {
 		this.korisnik = korisnik;
 	}
 
+	/**
+	 * 
+	 * @return metoda vraca vrednost atributa poruka
+	 */
+
 	public String getPoruka() {
 		return poruka;
 	}
+
+	/**
+	 * 
+	 * @param poruka
+	 *            metoda podesava vrednost atributa poruka na unetu vrednost
+	 */
 
 	public void setPoruka(String poruka) {
 		if (poruka == null || poruka.length() > 140)
@@ -24,6 +62,10 @@ public class TwitterPoruka {
 					"Poruka mora biti uneta i mora imati najvise 140 znakova");
 		this.poruka = poruka;
 	}
+
+	/**
+	 * toString metoda za atribute korisnik i poruka
+	 */
 
 	public String toString() {
 		return "KORISNIK:" + korisnik + " PORUKA:" + poruka;
